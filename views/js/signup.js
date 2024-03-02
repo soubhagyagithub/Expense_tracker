9fbea15abdf467a6f9c2f06f23cbe85d60656b07
Soubhagya Ranjan Sethi <soubhagya.edu12@gmail.com>
2024-03-02 18:28:46 +0530

    signup frontend

---
+++ views/js/signup.js
@@ -0,0 +1,25 @@
+function createNewUser(event){
+    event.preventDefault();
+
+    const username = event.target.userName.value;
+    const useremail = event.target.userEmail.value;
+    const userpassword = event.target.userPassword.value;
+    const userpasswordrepeat = event.target.userPasswordRepeat.value;
+
+    const userDetails = {
+        username: username,
+        useremail: useremail,
+        userpassword: userpassword,
+        userpasswordrepeat: userpasswordrepeat
+    };
+    console.log(userDetails);
+
+    try {
+        axios.post('http://localhost:3000/user/signup', userDetails)
+        .then( (result) = () => {
+            console.log(result)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
