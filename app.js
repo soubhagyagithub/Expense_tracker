dca05e9a716de545310424aa4930ad42b6cfb889
Soubhagya Ranjan Sethi <93478954+soubhagyagithub@users.noreply.github.com>
2025-01-12 20:02:43 +0530

    fixed header

--- app.js
+++ app.js
@@ -4,20 +4,23 @@ const path = require("path");
 const fs = require("fs");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const helmet = require("helmet");
 const morgan = require("morgan");
 
 dotenv.config();
 const sequelize = require("./util/database");
 
-app.use(cors());
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
+//log file
 const accessLogStream = fs.createWriteStream(
   path.join(__dirname, "access.log"),
   { flags: "a" }
 );
+
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(helmet());
 app.use(morgan("combined", { stream: accessLogStream }));
 
 // Routers
